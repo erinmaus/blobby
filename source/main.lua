92f7cc2ef47eaf7a0de8c9722a1f5ef64139a23c
Aaron Bolyard <adb@aaronbolyard.com>
2016-05-19 16:32:54 -0400

    Added main logic loop and a few commands.
    * Added logic loop to use commands on the database.
    * Implemented 'add', 'remove', 'list', and 'exit' commands.
    * Fixed copy-paste in the file header for 'main.lua'.

--- source/main.lua
+++ source/main.lua
@@ -1,5 +1,4 @@
--- Commands interface. A simple way to handle dispatching commands and parse
--- arguments.
+-- Blobby application implementation.
 --
 -- This file is licensed under the BSD 2-Clause license.
 --
@@ -14,6 +13,137 @@ end
 
 local function write_line(...)
 	io.write(string.format(...), "\n")
+end
+
+local function read_line(...)
+	write(...)
+
+	return io.read("*l")
+end
+
+local function require_argument(name, v)
+	if not v then
+		error(string.format("argument '%s' required", name))
+	end
+end
+
+local function validate_enum(v, e)
+	local match = false
+	for i = 1, #e do
+		if v == e[i] then
+			match = true
+
+			break
+		end
+	end
+
+	if not match then
+		local m = string.format(
+			"value '%s' is not valid; must be one of:\n\t%s",
+			v,
+			table.concat(e, ", "))
+		error(m)
+	end
+end
+
+local Confirm = command.create()
+function Confirm.yes()
+	return true
+end
+
+function Confirm.no()
+	return false
+end
+
+function ask_confirmation(message)
+	write_line(message)
+
+	local success, result
+	while not success do
+		local c = read_line("Enter 'yes' to confirm, 'no' to cancel: ")
+
+		success, result = Confirm(c)
+
+		if not success then
+			write_line("Invalid option.")
+		end
+	end
+
+	return result
+end
+
+local State = {}
+local Session = command.create()
+function Session.list(field, pattern)
+	field = field or 'all'
+
+	validate_enum(field, { 'all', 'domain', 'username', 'category' })
+
+	local success, result = State.database:query_list(field, pattern)
+	if not success then
+		write_line("Error: %s", result)
+	end
+
+	write_line("%20s %20s %20s", "username", "domain", "category")
+
+	local matches = 0
+	local entry = result()
+	while entry do
+		write_line(
+			"%20s %20s %20s",
+			entry.username,
+			entry.domain,
+			entry.category)
+		matches = matches + 1
+		entry = result()
+	end
+
+	write_line("\nFound %d matches.", matches)
+end
+
+function Session.add(username, domain, category)
+	require_argument("username", username)
+	require_argument("domain", domain)
+
+	local success, entry = State.database:add_entry(username, domain, category)
+
+	if success then
+		State.active = entry
+		write_line("Entry successfully created.")
+	else
+		write_line("Entry already exists in database.")
+	end
+end
+
+function Session.remove()
+	if State.active == nil then
+		write_line("No active entry. Select an entry and then try again.")
+	else
+		local m = string.format(
+			"Are you sure you want to remove '%s' from '%s'?",
+			State.active.username,
+			State.active.domain)
+
+		if ask_confirmation(m) then
+			local result = State.database:remove_entry(State.active)
+
+			if not result then
+				write_line("Entry was not in database.")
+			end
+
+			State.active = nil
+		else
+			write_line("No action taken.")
+		end
+	end
+end
+
+function Session.exit()
+	if ask_confirmation("Save any changes?") then
+		State.database:save(State.database.path)
+	end
+
+	return 'exit'
 end
 
 local Main = command.create()
@@ -50,7 +180,11 @@ function Main.open(path)
 
 	if not success then
 		return false, "Unable to open database. Wrong passphrase?"
+	else
+		write_line("Successfully opened '%s'.", path)
 	end
+
+	crypto.free_passphrase(passphrase)
 
 	return true, d
 end
@@ -64,7 +198,24 @@ function Main.help()
 	return false, nil
 end
 
-local function session_loop()
+local function session_loop(d, path)
+	State.database = d
+
+	local is_running = true
+	while is_running do
+		local c = read_line("> ")
+
+		local success, result = Session(command.parse_arguments(c))
+		if not success then
+			write_line("Error: %s", result)
+		end
+
+		if result == 'exit' then
+			is_running = false
+		end
+	end
+
+	State.database:destroy()
 end
 
 local function start(...)
